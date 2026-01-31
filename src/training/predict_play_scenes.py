c6b79b050b682820fe24799f334dae770d7cf1a8
TakuM-M <makkein3830@gmail.com>
2026-01-31 20:57:56 +0900

    feat:推論

--- src/training/predict_play_scenes.py
+++ src/training/predict_play_scenes.py
@@ -16,7 +16,7 @@ from tqdm import tqdm
 
 sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 from src.models.play_classifier_lstm import PlayClassifierLSTM, PlayClassifierCNNLSTM
-from src.dataset.dataset import PoseSequenceDataset
+from src.datasets import PoseSequenceDataset
 
 
 class PlayScenePredictor:
