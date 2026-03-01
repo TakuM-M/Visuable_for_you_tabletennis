cbec6cfa1dbe7c084711e0ab95118869ee1a2a7d
TakuM-M <makkein3830@gmail.com>
2026-03-01 17:45:26 +0900

    feat: frontendの環境構築

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",   // Dockerコンテナ外からアクセスできるようにする
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://backend:8000",  // docker-compose のサービス名
+        changeOrigin: true,
+      },
+    },
+  },
+})
